512c963e0b8c481df06591cfb9a5c330d8850c53
JANEMENG21 <yumeng.zhu21@gmail.com>
2025-02-13 23:26:45 +0800

    FINISHED

--- public/javascripts/fileUpload.js
+++ public/javascripts/fileUpload.js
@@ -1,12 +1,29 @@
-FilePond.registerPlugin(
-    FilePondPluginImagePreview,
-    FilePondPluginImageResize,
-    FilePondPluginFileEncode,
-  )
-FilePond.setOptions({
-    stylePanelAspectRatio: 150/100, //inside the documentation https://pqina.nl/filepond/docs/api/instance/properties/
-    imageResizeTargetWidth: 100,
-    imageResizeTargetHeight: 150
-}) 
+const rootStyles = window.getComputedStyle(document.documentElement)
 
-FilePond.parse(document.body)//all into filepond objects
+if (rootStyles.getPropertyValue ('--book-cover-width-large') != null &&  rootStyles.getPropertyValue('--book-cover-width-large') !== '') {
+  ready()
+} else{
+  document.getElementById('main-css').addEventListener('load',ready)
+}
+// check whether main.css is loaded
+
+function ready(){
+  const coverWidth = parseFloat(rootStyles.getPropertyValue('--book-cover-width-large'))
+  const coverAspectRatio = parseFloat(rootStyles.getPropertyValue('--book-cover-aspect-ratio'))
+  const coverHeight = coverWidth / coverAspectRatio
+    FilePond.registerPlugin(
+        FilePondPluginImagePreview,
+        FilePondPluginImageResize,
+        FilePondPluginFileEncode,
+      )
+      
+    FilePond.setOptions({
+        stylePanelAspectRatio: 1 / coverAspectRatio, //inside the documentation https://pqina.nl/filepond/docs/api/instance/properties/
+        imageResizeTargetWidth: coverWidth,
+        imageResizeTargetHeight: coverHeight
+    }) 
+
+    FilePond.parse(document.body)//all into filepond objects
+}
+
+  
